92857e212e4832c4eeeb93a22a182ee5d9e89c16
joshytheprogrammer <jurassicjoshua@protonmail.com>
2022-09-06 13:52:16 +0100

    made changes

--- pages/index.vue
+++ pages/index.vue
@@ -1,18 +1,18 @@
 <template>
   <div class="home">
     <Hero />
-    <Products />
+    <Product />
   </div>
 </template>
 
 <script>
 import Hero from "../components/Home/Hero.vue"
-import Products from "../components/Home/Products.vue"
+import Product from "../components/Home/Skirt.vue"
 
 export default {
   components: {
     Hero,
-    Products
+    Product
   }
 }
 </script>
