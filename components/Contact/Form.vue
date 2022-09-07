d2bc142c19d320ff655b898beeeda3b0743eed8d
joshytheprogrammer <jurassicjoshua@protonmail.com>
2022-09-07 15:15:36 +0100

    made changes: ''

---
+++ components/Contact/Form.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="contact">
+    <div class="form">
+      <h2>Reach out !</h2>
+    </div>
+    <div class="hub">
+
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss" scoped>
+.contact {
+  display: flex;
+  justify-content: space-around;
+
+
+}
+</style>
