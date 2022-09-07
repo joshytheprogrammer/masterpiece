9f68eeaaace9090a879f7614454ec37cecde6b76
joshytheprogrammer <jurassicjoshua@protonmail.com>
2022-09-07 16:05:57 +0100

    made changes: ''

--- components/Contact/Form.vue
+++ components/Contact/Form.vue
@@ -5,7 +5,13 @@
       <p>We’d love to hear from you and answer any questions you might have. Your feedback is very important to us. Please leave us a message with all your inquiries, observations, suggestions, or complaints.</p>
 
       <form>
-
+        <input type="text" class="form-control h" placeholder="First Name">
+        <input type="text" class="form-control h" placeholder="Last Name">
+        <input type="text" class="form-control h" placeholder="Email">
+        <input type="text" class="form-control h" placeholder="Phone">
+        <input type="text" class="form-control f" placeholder="Subject">
+        <textarea class="form-control text f" placeholder="Message"></textarea>
+        <button>Send Message</button>
       </form>
     </div>
     <div class="hub">
@@ -39,12 +45,56 @@ export default {
       font-size: 48px;
       font-weight: 500;
       color: $d-secondary;
-      text-shadow: 1px 1px $secondary;
     }
 
     p {
-      font-size: 18px;
+      font-size: 16px;
       line-height: 1.3;
+    }
+
+    form {
+      width: 100%;
+      display: flex;
+      flex-wrap: wrap;
+      justify-content: space-between;
+
+      .form-control {
+        margin: 0.5rem 0;
+        background: transparent;
+        border: none;
+        height: 40px;
+        padding: 2px 4px;
+        border-bottom: 1px solid $d-secondary;
+
+        font: {
+          size: 16px;
+          weight: 400;
+        }
+
+        &::placeholder {
+          font: {
+            size: 16px;
+            weight: 300;
+          }
+        }
+
+        &:focus {
+          outline: none;
+          border-bottom-color: $secondary;
+        }
+
+        &.h {
+          width: 49%;
+        }
+
+        &.f {
+          width: 100%;
+        }
+      }
+
+      textarea.text {
+        height: 120px;
+      }
     }
   }
 
