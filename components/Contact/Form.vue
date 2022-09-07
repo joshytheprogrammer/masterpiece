20cff36c7264c8ad25342b420cab8ac4e3f6c922
joshytheprogrammer <jurassicjoshua@protonmail.com>
2022-09-07 15:37:25 +0100

    made changes: ''

--- components/Contact/Form.vue
+++ components/Contact/Form.vue
@@ -2,7 +2,7 @@
   <div class="contact">
     <div class="form">
       <h2>Reach out!</h2>
-      <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nesciunt dignissimos nobis fuga culpa doloribus odit esse, consectetur obcaecati placeat dolor rem quasi laborum? Magni, obcaecati a. Deleniti necessitatibus eligendi maxime?</p>
+      <p>We’d love to hear from you and answer any questions you might have. Your feedback is very important to us. Please leave us a message with all your inquiries, observations, suggestions, or complaints.</p>
 
       <form>
 
@@ -23,16 +23,34 @@ export default {
 <style lang="scss" scoped>
 .contact {
   display: flex;
+  flex-wrap: wrap;
   justify-content: center;
+  align-items: center;
 
   .form {
+    padding: 0.4rem 1rem;
+    max-width: 600px;
+
+    h2, p {
+      padding-bottom: 1rem;
+    }
 
     h2 {
-      
+      font-size: 48px;
+      font-weight: 500;
+      color: $d-secondary;
+      text-shadow: 1px 1px $secondary;
+    }
+
+    p {
+      font-size: 18px;
+      line-height: 1.3;
     }
   }
 
   .hub {
+    width: 500px;
+    height: 800px;
     background-color: transparent;
     background-image: radial-gradient(at center center, $secondary 0%, $d-secondary 100%);
 
