7ec6ddf6751ef1e5728feda9ba1edbb8d5a95d07
joshytheprogrammer <jurassicjoshua@protonmail.com>
2022-09-08 11:02:15 +0100

    made changes: ''

--- components/Contact/Socials.vue
+++ components/Contact/Socials.vue
@@ -17,6 +17,7 @@ export default {
 <style lang="scss" scoped>
 .socials {
   padding: 2rem 1rem;
+  height: 500px;
   color: $light;
   background-image: url('https://www.nbplc.com/storage/2021/10/glass-cold-beer-macro-photography-min-scaled.jpg');
   background-attachment: fixed;
@@ -26,6 +27,30 @@ export default {
   display: flex;
   flex-direction: column;
   justify-content: center;
+  align-items: center;
   text-align: center;
+
+  h2 {
+    padding: 1rem 0;
+    font-size: 48px;
+    font-weight: 500;
+    color: $secondary;
+    text-shadow: 2px 3px 1px $dark;
+  }
+
+  p {
+    padding: 1rem 0;
+    font-size: 20px;
+    width: 480px;
+    font-weight: 400;
+    line-height: 25px;
+  }
+
+  @media screen and (max-width: $medium){
+    p {
+      padding: 0 1rem;
+      width: 100%;
+    }
+  }
 }
 </style>
