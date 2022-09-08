56d5acb6d337654276f3be646a8b087208ca9ab6
joshytheprogrammer <jurassicjoshua@protonmail.com>
2022-09-08 16:20:52 +0100

    made changes: ''

--- components/Products/Product.vue
+++ components/Products/Product.vue
@@ -1,10 +1,20 @@
 <template>
-  
+  <div class="products">
+    <div class="product">
+
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
+  data() {
+    return {
+      products: {
 
+      }
+    }
+  }
 }
 </script>
 
