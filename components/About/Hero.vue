9717a800335b7adb37d8167b998cfca57777f674
joshytheprogrammer <jurassicjoshua@protonmail.com>
2022-09-09 17:10:54 +0100

    made changes: ''

--- components/About/Hero.vue
+++ components/About/Hero.vue
@@ -12,31 +12,15 @@ export default {
 
 <style lang="scss" scoped>
 .hero {
-  height: 400px;
+  @include normal-hero-styles;
   background: radial-gradient(at center center, $trans-dark 0%, $dark 100%);
 
-  color: $light;
-
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  text-align: center;
-
   h1 {
-    padding: 1rem;
-    font-size: 48px;
-    font-weight: 400;
     text-shadow: 2px 3px 1px $trans-dark;
 
     span {
-      color: $secondary;
       text-shadow: 2px 3px 1px $trans-dark;
     }
   }
-
-  @media screen and (max-width: $medium){
-    height: 500px;
-  }
-
 }
 </style>
--- components/Contact/Hero.vue
+++ components/Contact/Hero.vue
@@ -12,33 +12,11 @@ export default {
 
 <style lang="scss" scoped>
 .hero {
-  height: 400px;
+  @include normal-hero-styles;
+
   background-image: url('https://images.unsplash.com/photo-1534536281715-e28d76689b4d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80&h=500');
   background-attachment: fixed;
   background-repeat: no-repeat;
   background-color: $dark;
-  color: $light;
-
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  text-align: center;
-
-  h1 {
-    padding: 1rem;
-    font-size: 48px;
-    font-weight: 400;
-    text-shadow: 2px 3px 1px $dark;
-
-    span {
-      color: $secondary;
-      text-shadow: 2px 3px 1px $dark;
-    }
-  }
-
-  @media screen and (max-width: $medium){
-    height: 500px;
-  }
-
 }
 </style>
