b3d8e8bcf6d75413ce7c41e453ab206799dd4289
joshytheprogrammer <jurassicjoshua@protonmail.com>
2022-09-09 16:09:19 +0100

    made changes: ''

---
+++ components/About/Hero.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="hero">
+    <h1>About this <span>Masterpiece</span></h1>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss" scoped>
+.hero {
+  height: 400px;
+  background: radial-gradient(at center center, $light 0%, $dark 100%);
+  color: $light;
+
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  text-align: center;
+
+  h1 {
+    padding: 1rem;
+    font-size: 48px;
+    font-weight: 400;
+    text-shadow: 2px 3px 1px $trans-dark;
+
+    span {
+      color: $secondary;
+      text-shadow: 2px 3px 1px $trans-dark;
+    }
+  }
+
+  @media screen and (max-width: $medium){
+    height: 500px;
+  }
+
+}
+</style>
--- pages/about.vue
+++ pages/about.vue
@@ -1,10 +1,15 @@
 <template>
-  
+  <div class="about">
+    <Hero />
+  </div>
 </template>
 
 <script>
+import Hero from "../components/About/Hero.vue"
 export default {
-
+  components: {
+    Hero
+  }
 }
 </script>
 
