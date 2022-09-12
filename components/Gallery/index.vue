00abc604d170e80cb6bbc47cfb03a4ccd0b064bc
joshytheprogrammer <jurassicjoshua@protonmail.com>
2022-09-12 15:10:04 +0100

    made changes: ''

--- components/Gallery/index.vue
+++ components/Gallery/index.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="gallery">
-    <figure>
-      <!-- <img src="../../assets/img/_DSC8878.png" alt=""> -->
+    <figure v-for="item in images" :key="item.id">
+      <img :src="item.link" :alt="item.alt">
     </figure>
   </div>
 </template>
@@ -11,28 +11,118 @@ export default {
   data() {
     return {
       images: [
-        "",
-        "",
-
+        {
+          id: 0,
+          alt: 'img-1',
+          link: 'https://res.cloudinary.com/dsgvwxygr/image/upload/v1662190088/masterpiece/MODO7364_hn0pra.jpg'
+        },
+        {
+          id: 1,
+          alt: 'img-2',
+          link: 'https://res.cloudinary.com/dsgvwxygr/image/upload/v1662457782/masterpiece/MODO7289_zxypmq.jpg'
+        },
+        {
+          id: 2,
+          alt: 'img-3',
+          link: 'https://res.cloudinary.com/dsgvwxygr/image/upload/v1662543202/masterpiece/MODO7289_qqhhe6_1f1e80.jpg'
+        },
+        {
+          id: 3,
+          alt: 'img-4',
+          link: 'https://res.cloudinary.com/dsgvwxygr/image/upload/v1662987910/masterpiece/bg-img-3_wigjxc.jpg'
+        },
+        {
+          id: 4,
+          alt: 'img-1',
+          link: 'https://res.cloudinary.com/dsgvwxygr/image/upload/v1658833457/cld-sample-4.jpg'
+        },
+        {
+          id: 5,
+          alt: 'img-2',
+          link: 'https://res.cloudinary.com/dsgvwxygr/image/upload/v1658833458/cld-sample-5.jpg'
+        },
+        {
+          id: 6,
+          alt: 'img-3',
+          link: 'https://res.cloudinary.com/dsgvwxygr/image/upload/v1658833457/cld-sample.jpg'
+        },
+        {
+          id: 7,
+          alt: 'img-4',
+          link: 'https://res.cloudinary.com/dsgvwxygr/image/upload/v1658833457/cld-sample-3.jpg'
+        },
+        {
+          id: 8,
+          alt: 'img-3',
+          link: 'https://res.cloudinary.com/dsgvwxygr/image/upload/v1658833457/cld-sample-2.jpg'
+        },
+        {
+          id: 9,
+          alt: 'img-4',
+          link: 'https://res.cloudinary.com/dsgvwxygr/image/upload/v1656162193/sample.jpg'
+        }
       ]
     }
+  },
+  mounted() {
+    this.images = this.shuffle(this.images)
+  },
+  methods: {
+    shuffle(array) {
+      let currentIndex = array.length,  randomIndex;
+
+      // While there remain elements to shuffle.
+      while (currentIndex != 0) {
+
+        // Pick a remaining element.
+        randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex--;
+
+        // And swap it with the current element.
+        [array[currentIndex], array[randomIndex]] = [
+          array[randomIndex], array[currentIndex]];
+      }
+
+      console.log(array)
+
+      return array;
+    }
+
   }
 }
 </script>
 
 <style lang="scss" scoped>
 .gallery {
-  display: flex;
-  flex-wrap: wrap;
-  justify-content: center;
-  align-items: baseline;
+  columns: 4 200px;
+  column-gap: 1.5rem;
+  padding: 1rem;
+  margin: 1rem auto;
 
   figure {
-    display: block;
-    max-width: 400px;
+    width: 150px;
+    margin: 0 1.5rem 1.5rem 0;
+    display: inline-block;
+    width: 100%;
+    border: solid 2px $dark;
+    padding: 5px;
+    box-shadow: 5px 5px 5px $trans-dark;
+    border-radius: 5px;
+    transition: all .25s ease-in-out;
+
+    &:hover img {
+      filter: grayscale(0);
+    }
+
+    &:hover {
+      border-color: $secondary;
+    }
 
     img {
-      width: 300px;
+      width: 100%;
+      filter: grayscale(100%);
+      border-radius: 5px;
+      transition: all .25s ease-in-out;
     }
   }
 }
