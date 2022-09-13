30bc627639624158266a4280669e07b362027249
joshytheprogrammer <jurassicjoshua@protonmail.com>
2022-09-13 11:48:04 +0100

    made changes:''

---
+++ components/About/Brewing.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="brewing">
+
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss" scoped>
+.brewing {
+  background-image: url('https://images.unsplash.com/photo-1535958636474-b021ee887b13?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&h=700&q=80');
+  height: 600px;
+}
+</style>
--- pages/about.vue
+++ pages/about.vue
@@ -2,16 +2,20 @@
   <div class="about">
     <Hero />
     <Company />
+    <Brewing />
   </div>
 </template>
 
 <script>
 import Hero from "../components/About/Hero.vue"
 import Company from "../components/About/Company.vue"
+import Brewing from "../components/About/Brewing.vue"
+
 export default {
   components: {
     Hero,
-    Company
+    Company,
+    Brewing
   }
 }
 </script>
