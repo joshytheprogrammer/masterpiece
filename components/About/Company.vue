dcaec127d24cf9eef9ad56e2d827e529420c59a1
joshytheprogrammer <jurassicjoshua@protonmail.com>
2022-09-13 11:36:36 +0100

    made changes:''

--- components/About/Company.vue
+++ components/About/Company.vue
@@ -5,7 +5,7 @@
       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur dignissimos harum sequi esse autem, nam reiciendis commodi! Ad, omnis quod esse dolore mollitia earum. Ex quo repudiandae quidem nostrum autem!</p>
     </div>
     <div class="_img">
-      <img src="https://images.unsplash.com/photo-1600788886242-5c96aabe3757?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80" alt="our-company-about-image">
+      <img src="https://images.unsplash.com/photo-1580397581145-cdb6a35b7d3f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2230&q=80" alt="our-company-about-image">
     </div>
   </div>
 </template>
@@ -28,14 +28,40 @@ export default {
     padding: 1rem;
 
     h2 {
-      font-size: 40px;
+      font-size: 48px;
       font-weight: 700;
+      padding: 1rem 0;
       color: $d-secondary;
     }
 
     p {
       font-size: 20px;
-      line-height: 1.24;
+      line-height: 1.5;
+    }
+  }
+
+  ._img {
+    max-width: 50%;
+
+    img {
+      width: 100%;
+    }
+  }
+
+  @media screen and (max-width: $medium){
+    flex-direction: column-reverse;
+
+    .copy h2 {
+      padding-top: 0;
+      font-size: 40px;
+    }
+
+    .copy p {
+      font-size: 16px;
+    }
+
+    ._img {
+      max-width: 100%;
     }
   }
 }
