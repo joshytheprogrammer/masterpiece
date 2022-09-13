9ef6f387dd09b6409341c78ad1ef5b2da36c513c
joshytheprogrammer <jurassicjoshua@protonmail.com>
2022-09-13 12:54:07 +0100

    made changes:''

---
+++ components/About/Lineup.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="lineup">
+    <img src="https://res.cloudinary.com/dsgvwxygr/image/upload/v1663069739/masterpiece/bulk-pics-transformed_hleb7s.png" alt="">
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss" scoped>
+.lineup {
+  padding-bottom: 2rem;
+
+  img {
+    width: 100%;
+    height: 500px;
+    object-fit: contain;
+
+    background: $dark;
+    padding: 2rem 0;
+  }
+}
+</style>
