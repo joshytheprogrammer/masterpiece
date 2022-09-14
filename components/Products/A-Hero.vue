2274e5dcea76fba53d96b2d7bf1f7c504ff61839
joshytheprogrammer <jurassicjoshua@protonmail.com>
2022-09-14 16:46:33 +0100

    made changes: ''

--- components/Products/A-Hero.vue
+++ components/Products/A-Hero.vue
@@ -19,5 +19,18 @@ export default {
 .after-hero {
   display: grid;
   grid-template-columns: 2fr 1fr;
+
+  .notes {
+    padding: 1rem;
+    background: $trans-dark;
+
+
+    h2 {
+      padding: 1rem 0;
+      font-size: 48px;
+      font-weight: 400;
+      color: $secondary;
+    }
+  }
 }
 </style>
