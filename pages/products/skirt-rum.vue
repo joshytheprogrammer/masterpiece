7f8f132c50306bef510e427920b41c1821463960
joshytheprogrammer <jurassicjoshua@protonmail.com>
2022-09-14 11:37:33 +0100

    made changes: ''

--- pages/products/skirt-rum.vue
+++ pages/products/skirt-rum.vue
@@ -1,13 +1,35 @@
 <template>
-  
+  <div class="kubanah">
+    <PHero :bgImg="bgImg" :name="name" :pitch="pitch" :image="image" />
+  </div>
 </template>
 
 <script>
+import PHero from "../../components/Products/P-Hero.vue"
 export default {
-
+  components: {
+    PHero
+  },
+  head() {
+    return {
+      title: 'Kubanah Whisky | Masterpiece Flavoured Spirit Drink.',
+      meta: [
+        // hid is used as unique identifier. Do not use `vmid` for it as it will not work
+        {
+          hid: 'description',
+          name: 'description',
+          content: 'A blend of grain spirits and scotch malts for exquisite smoothness. Masterpiece Flavoured Spirit Drink'
+        }
+      ]
+    }
+  },
+  data() {
+    return {
+      bgImg: "https://res.cloudinary.com/dsgvwxygr/image/upload/c_scale,h_900,w_1920/v1663079665/masterpiece/gradient-3_jyvdtn.png",
+      name: "Skirt Cafe Rum",
+      pitch: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Reprehenderit necessitatibus exercitationem.",
+      image: "https://res.cloudinary.com/dsgvwxygr/image/upload/v1662651185/masterpiece/MODO7480-transformed_p30qym.png"
+    }
+  },
 }
 </script>
-
-<style>
-
-</style>
