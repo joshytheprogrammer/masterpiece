b8d59748fdbbb028513bead39c25364cbda69898
joshytheprogrammer <jurassicjoshua@protonmail.com>
2022-09-14 17:53:42 +0100

    made changes: ''

--- pages/products/skirt-gin.vue
+++ pages/products/skirt-gin.vue
@@ -1,14 +1,17 @@
 <template>
   <div class="kubanah">
     <PHero :bgImg="bgImg" :name="name" :pitch="pitch" :image="image" />
+    <AHero :features="features" :name="name" :desc="description" />
   </div>
 </template>
 
 <script>
 import PHero from "../../components/Products/P-Hero.vue"
+import AHero from "../../components/Products/A-Hero.vue"
 export default {
   components: {
-    PHero
+    PHero,
+    AHero
   },
   head() {
     return {
@@ -27,7 +30,15 @@ export default {
       bgImg: "https://res.cloudinary.com/dsgvwxygr/image/upload/c_scale,h_900,w_1980/v1663079973/masterpiece/gradient-5_k1oya8.png",
       name: "Skirt Dry Gin",
       pitch: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis, facilis qui. Eius impedit sit suscipit fugiat, sunt perspiciatis necessitatibus praesentium.",
-      image: "https://res.cloudinary.com/dsgvwxygr/image/upload/v1662987025/masterpiece/MODO7490-transformed_i2ef7m.png"
+      description: "",
+      image: "https://res.cloudinary.com/dsgvwxygr/image/upload/v1662987025/masterpiece/MODO7490-transformed_i2ef7m.png",
+      features: {
+        ingredients: '',
+        flavour: '',
+        palate: '',
+        abv: '40%',
+        app: ''
+      }
     }
   },
 }
--- pages/products/skirt-rum.vue
+++ pages/products/skirt-rum.vue
@@ -1,14 +1,17 @@
 <template>
   <div class="kubanah">
     <PHero :bgImg="bgImg" :name="name" :pitch="pitch" :image="image" />
+    <AHero :features="features" :name="name" :desc="description" />
   </div>
 </template>
 
 <script>
 import PHero from "../../components/Products/P-Hero.vue"
+import AHero from "../../components/Products/A-Hero.vue"
 export default {
   components: {
-    PHero
+    PHero,
+    AHero
   },
   head() {
     return {
@@ -27,7 +30,15 @@ export default {
       bgImg: "https://res.cloudinary.com/dsgvwxygr/image/upload/c_scale,h_900,w_1920/v1663079665/masterpiece/gradient-3_jyvdtn.png",
       name: "Skirt Cafe Rum",
       pitch: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Reprehenderit necessitatibus exercitationem.",
-      image: "https://res.cloudinary.com/dsgvwxygr/image/upload/v1662651185/masterpiece/MODO7480-transformed_p30qym.png"
+      description: "",
+      image: "https://res.cloudinary.com/dsgvwxygr/image/upload/v1662651185/masterpiece/MODO7480-transformed_p30qym.png",
+      features: {
+        ingredients: '',
+        flavour: '',
+        palate: '',
+        abv: '42%',
+        app: ''
+      }
     }
   },
 }
