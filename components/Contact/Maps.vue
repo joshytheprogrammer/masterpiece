3e46b4c01cd53f15daa0230174114bab51ec323e
joshytheprogrammer <jurassicjoshua@protonmail.com>
2022-09-14 15:16:51 +0100

    made changes: ''

--- components/Contact/Maps.vue
+++ components/Contact/Maps.vue
@@ -1,5 +1,5 @@
 <template>
   <div class="maps">
-    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3964.1344349925002!2d3.330563614902981!3d6.5046612251864!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x103b8e98c67b5957%3A0x9375b7a57075271c!2sMaster%20Piece%20Hotel!5e0!3m2!1sen!2sng!4v1662629494312!5m2!1sen!2sng" width="100%" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d4196.394752153225!2d3.331534324820692!3d6.506239910134897!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x103b8e9efc30235b%3A0x464d983e74f3b7b4!2s5%20Yisa%20St%2C%20Itire%20101241%2C%20Lagos!5e1!3m2!1sen!2sng!4v1663162215073!5m2!1sen!2sng" width="100%" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
   </div>
 </template>
