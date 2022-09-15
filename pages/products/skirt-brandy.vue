1808d825ae583d30abea8a22e866ff84f1a0eda1
joshytheprogrammer <jurassicjoshua@protonmail.com>
2022-09-15 10:49:12 +0100

    made changes: ''

--- pages/products/skirt-brandy.vue
+++ pages/products/skirt-brandy.vue
@@ -29,13 +29,14 @@ export default {
     return {
       bgImg: "https://res.cloudinary.com/dsgvwxygr/image/upload/c_scale,h_900,w_1920/v1663080559/masterpiece/gradient-4_talend.png",
       name: "Skirt Blended Brandy",
-      pitch: "Skirt Blended Brandy us distilled with fine spirits and aged by traditional skirt system in the oak butts.",
-      description: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sequi quo odit commodi dolor quae cumque nesciunt magnam aspernatur? Eius error ratione ea? A at facilis deserunt! Ipsum quia quam impedit. Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sequi quo odit commodi dolor quae cumque nesciunt magnam aspernatur? Eius error ratione ea? A at facilis deserunt! Ipsum quia quam impedit.",
+      pitch: "Skirt Blended Brandy is distilled with fine spirits and aged by traditional skirt system in the oak butts.",
+      description: "Skirt Brandy is distilled with fine spirits and aged by the traditional Skirt system in the oak butts. It retains its old tradition of high quality and is blended with flavour. It is a drink for all occasions whether on rocks, straight or mixed.",
       image: "https://res.cloudinary.com/dsgvwxygr/image/upload/v1662985746/masterpiece/skirt-blended-brandy_t7f603.png",
       features: {
-        ingredients: '',
-        flavour: '',
-        palate: '',
+        ingredients: 'Treated Water, Ethanol, Brandy Flavour and Caramel',
+        aroma: 'Skirt Blended Brandy with fine spirits and oak butts',
+        flavour: 'A refreshing blend of Brandy Flavour and Caramel for a sharp and refreshing taste',
+        palate: 'Quality tasting blended brandy',
         abv: '40%',
         app: ''
       }
