8b9b80d26dc93171555d24a42b5bc80dfc921b52
joshytheprogrammer <jurassicjoshua@protonmail.com>
2022-10-29 13:39:13 +0100

    improved contact

--- components/Contact/Maps.vue
+++ components/Contact/Maps.vue
@@ -1,5 +1,5 @@
 <template>
   <div class="maps">
-    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d4196.394752153225!2d3.331534324820692!3d6.506239910134897!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x103b8e9efc30235b%3A0x464d983e74f3b7b4!2s5%20Yisa%20St%2C%20Itire%20101241%2C%20Lagos!5e1!3m2!1sen!2sng!4v1663162215073!5m2!1sen!2sng" width="100%" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d19619.5530308471!2d3.1802393352026366!3d6.885164673407842!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x103ba5ce806a8447%3A0xc8b45176df2662df!2s110106%2C%20Papalanto!5e1!3m2!1sen!2sng!4v1667047080163!5m2!1sen!2sng" width="100%" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
   </div>
 </template>
