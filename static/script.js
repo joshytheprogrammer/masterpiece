66e75f0e552ef737eeeb76835998883aa3a58a02
Joshua <jurassicjoshua@protonmail.com>
2023-10-11 16:41:43 +0100

    made changes: ''

--- static/script.js
+++ static/script.js
@@ -3,3 +3,5 @@ function gtag(){dataLayer.push(arguments);}
 gtag('js', new Date());
 
 gtag('config', 'G-GXWG91EZTV');
+
+gtag('event', 'conversion', {'send_to': 'AW-11344296584/dvhJCIrwi-sYEIj1sKEq'});
