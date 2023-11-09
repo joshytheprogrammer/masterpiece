3c90dd8b21e2c6a7283833bed3f166bc52bbdea4
Joshua <jurassicjoshua@protonmail.com>
2023-11-09 19:36:26 +0100

    made changes: 'added keywords'

--- pages/index.vue
+++ pages/index.vue
@@ -17,6 +17,18 @@ export default {
     Product,
     Kubanah
   },
+  head() {
+    return {
+      meta: [
+        {
+          hid: 'keywords',
+          name: 'keywords',
+          content: "whiskey stores in Lagos, whiskey shop Lekki Lagos, beer store Lagos, alcohol store Lagos, spirit drink delivery Lagos, Kubanah Whisky store Lagos, wine shop Lagos, beer store Lagos, alcohol store Lagos, whisky prices Nigeria, whiskey drink delivery lagos Nigeria,spirit drink shop Lagos, whisky store Lagos, whisky shop Lagos, Kubanah Whisky store Lagos, whiskey shop Lagos Kubanah Whisky delivery Lagos, alcohol delivery Lagos, online whisky delivery Lagos, Kubanah whisky store near me Kubanah Whisky store Lagos, Kubanah Whisky shop Lagos, beer store Lagos, alcohol store Lagos, beer prices Nigeria Kubanah Whisky store Lagos, wine shop Lagos, beer store Lagos, alcohol store Lagos, Kubanah Whisky store near me Kubanah Whisky Nigeria, Kubanah Whisky price, Kubanah Whisky Lagos, Kubanah Whisky price in Nigeria, Kubanah Whiskey price in Lagos, Whiskey price in Ogun, Price of Whiskey in Imo whiskey shop Lagos, beer store Lagos, alcohol store Lagos, whisky prices Nigeria",
+        },
+        // Other meta tags as needed
+      ],
+    };
+  },
 }
 </script>
 
